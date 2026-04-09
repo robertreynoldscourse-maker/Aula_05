771af765a141eb8dc2214437d9726d8df36e6c46
ROBERT REYNOLDS RODRIGUEZ FLORES <robert.flores@aluno.senac.rio>
2026-04-09 18:51:14 -0300

    Subindo a segunda modificação em exemplo01.py

--- aula05/exemplo01.py
+++ aula05/exemplo01.py
@@ -1,3 +1,6 @@
+
+## IF encadeado
+print('IF encadeado')
 nota = float(input('Informe a nota do aluno: '))
 
 if nota >= 9:
@@ -7,6 +10,21 @@ elif nota >= 7:
 elif nota >= 5:
     print('C')
 elif nota >= 3:
+    print('D')
+else:
+    print('E')
+
+print('------------------------------------------')
+# IF não encadeado
+print('IF não encadeado')
+nota = float(input('Informe a nota do aluno: '))
+if nota >= 9:
+    print('A')
+if nota >= 7:
+    print('B')
+if nota >= 5:
+    print('C')
+if nota >= 3:
     print('D')
 else:
     print('E')
---
+++ aula05/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+nota = float(input('Informe a nota do aluno: '))
+if nota >= 9:
+    print('A')
+if nota >= 7:
+    print('B')
+if nota >= 5:
+    print('C')
+if nota >= 3:
+    print('D')
+else:
+    print('E')
