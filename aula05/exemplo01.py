cb5ce97b3d652a93efdefb867a2a7d6e2d0c8a2d
ROBERT REYNOLDS RODRIGUEZ FLORES <robert.flores@aluno.senac.rio>
2026-04-09 18:43:42 -0300

    Subindo a primeira atividade da aula05

---
+++ aula05/exemplo01.py
@@ -0,0 +1,12 @@
+nota = float(input('Informe a nota do aluno: '))
+
+if nota >= 9:
+    print('A')
+elif nota >= 7:
+    print('B')
+elif nota >= 5:
+    print('C')
+elif nota >= 3:
+    print('D')
+else:
+    print('E')
