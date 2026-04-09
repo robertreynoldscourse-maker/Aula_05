6535272c19c278e61b94b1b8a393f25caabb7bb1
ROBERT REYNOLDS RODRIGUEZ FLORES <robert.flores@aluno.senac.rio>
2026-04-09 19:07:39 -0300

    Subindo o IF aninhado

--- aula05/exemplo01.py
+++ aula05/exemplo01.py
@@ -28,3 +28,21 @@ if nota >= 3:
     print('D')
 else:
     print('E')
+
+print('------------------------------------------')
+
+# IF aninhados
+
+nota_aluno = float(input('Informe a nota do aluno: '))
+frequencia = float(input('Informe a frequência do aluno: '))
+
+porcentagem_frequencia = frequencia / 100
+
+if nota_aluno >= 7:
+    # Aprovado por nota, mas precisa checar a frequência
+    if porcentagem_frequencia >= 0.75:
+        print('Aluno aprovado por Nota e Frequência.')
+    else:
+        print('Reprobado por frequência baixa.')
+else:
+    print('Reprobado por nota baixa.')
--- aula05/tempCodeRunnerFile.py
+++ aula05/tempCodeRunnerFile.py
@@ -1,11 +1,14 @@
-nota = float(input('Informe a nota do aluno: '))
-if nota >= 9:
-    print('A')
-if nota >= 7:
-    print('B')
-if nota >= 5:
-    print('C')
-if nota >= 3:
-    print('D')
+
+nota_aluno = float(input('Informe a nota do aluno: '))
+frequencia = float(input('Informe a frequência do aluno: '))
+
+porcentagem_frequencia = frequencia / 100
+
+if nota_aluno >= 7:
+    # Aprovado por nota, mas precisa checar a frequência
+    if porcentagem_frequencia >= 0.75:
+        print('Aluno aprovado por Nota e Frequência.')
+    else:
+        print('Reprobado por frequência baixa.')
 else:
-    print('E')
+    print('Reprobado por nota baixa.')
